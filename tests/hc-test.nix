926b277a9f5fe90226e8e751572510ab0be16081
samrose <samuel.rose@gmail.com>
2019-06-17 17:39:25 -0400

    testing hc, holochain, n3h

--- tests/hc-test.nix
+++ tests/hc-test.nix
@@ -12,6 +12,8 @@
       startAll;
       $machine->waitForUnit('holochain.service');
       $machine->succeed('hc -V') =~ /0.0.18-alpha1/ or die;
+      $machine->succeed('holochain -V') =~ /0.0.18-alpha1/ or die;
+      $machine->succeed('n3h -V') =~ /v0.0.13-alpha/ or die;
       $machine->shutdown;
     '';
 }
