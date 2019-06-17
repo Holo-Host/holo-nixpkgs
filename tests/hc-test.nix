7211c90b45155e810e99d17aca85fdc1cca79f47
samrose <samuel.rose@gmail.com>
2019-06-17 17:30:45 -0400

    pass test

--- tests/hc-test.nix
+++ tests/hc-test.nix
@@ -12,6 +12,6 @@
       startAll;
       $machine->waitForUnit("multi-user.target");
       $machine->waitForUnit('holochain.service');
-      $machine->succeed('hc -V') =~ /0.0.19-alpha1/ or die;
+      $machine->succeed('hc -V') =~ /0.0.18-alpha1/ or die;
     '';
 }
