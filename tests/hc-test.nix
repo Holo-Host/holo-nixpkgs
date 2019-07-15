e0971d69b8ccc4edd8201cce319ac08cf8244e6d
samrose <samuel.rose@gmail.com>
2019-07-15 15:55:58 -0400

    update tests

--- tests/hc-test.nix
+++ tests/hc-test.nix
@@ -13,9 +13,9 @@
       $machine->waitForUnit('holochain.service');
       $machine->waitForUnit('envoy.service');
       $machine->waitForUnit("multi-user.target");
-      $machine->succeed('hc -V') =~ /0.0.18-alpha1/ or die;
-      $machine->succeed('holochain -V') =~ /0.0.18-alpha1/ or die;
-      $machine->succeed('n3h -V') =~ /v0.0.13-alpha/ or die;
+      $machine->succeed('hc -V') =~ /0.0.24-alpha2/ or die;
+      $machine->succeed('holochain -V') =~ /0.0.24-alpha2/ or die;
+      $machine->succeed('n3h -V') =~ /v0.0.17-alpha/ or die;
       $machine->shutdown;
     '';
 }
