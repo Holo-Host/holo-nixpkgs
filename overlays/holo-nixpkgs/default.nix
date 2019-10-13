9d4e4ecb91757cd85557ea6e40cbf2816610bd15
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-10-13 04:23:43 +0000

    holo-whitelist -> holo-auth-client

--- overlays/holo-nixpkgs/default.nix
+++ overlays/holo-nixpkgs/default.nix
@@ -129,6 +129,11 @@ in
     linux = linux_latest;
   };
 
+  holo-auth-client = callPackage ./holo-auth-client {
+    stdenv = stdenvNoCC;
+    python3 = python3.withPackages (ps: [ ps.requests ]);
+  };
+
   holo-init = callPackage ./holo-init {
     stdenv = stdenvNoCC;
     python3 = python3.withPackages (ps: [ ps.requests ps.retry ]);
@@ -142,11 +147,6 @@ in
   holo-nixpkgs-tests = recurseIntoAttrs (import ../../tests {
     inherit pkgs;
   });
-
-  holo-whitelist = callPackage ./holo-whitelist {
-    stdenv = stdenvNoCC;
-    python3 = python3.withPackages (ps: [ ps.requests ]);
-  };
  
   holoportos = recurseIntoAttrs {
     profile = tryDefault <nixos-config> ../../profiles/holoportos;
--- overlays/holo-nixpkgs/holo-auth-client/default.nix
+++ overlays/holo-nixpkgs/holo-auth-client/default.nix
@@ -3,14 +3,14 @@
 with stdenv.lib;
 
 stdenv.mkDerivation {
-  name = "holo-whitelist";
+  name = "holo-auth-client";
 	
   nativeBuildInputs = [ makeWrapper ];
   buildInputs = [ python3 ];
 
   buildCommand = ''
-    makeWrapper ${python3}/bin/python3 $out/bin/holo-whitelist \
-      --add-flags ${./holo-whitelist.py} 
+    makeWrapper ${python3}/bin/python3 $out/bin/holo-auth-client \
+      --add-flags ${./holo-auth-client.py} 
   '';
 
   meta.platforms = platforms.linux;
