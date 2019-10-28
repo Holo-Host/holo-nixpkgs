6c5491909b4fa1a4a1d7864fa204c1e5f28a6a1f
zo-el <joelulahanna@gmail.com>
2019-10-28 00:03:07 -0500

    fix dnaPackage.holofuel

--- overlays/holo-nixpkgs/dna-packages/holofuel/default.nix
+++ overlays/holo-nixpkgs/dna-packages/holofuel/default.nix
@@ -2,9 +2,9 @@
 
 let
   src = fetchurl {
-    url = "https://holo-host.github.io/holofuel/releases/download/0.9.7-alpha1/holofuel.dna.json";
+    url = "https://holo-host.github.io/holofuel/releases/download/v0.11.1-alpha1/holofuel.dna.json";
     name = "holofuel.dna.json";
-    sha256 = "0cgl88izvj4gnjvv21j8fz0fax3l45gmf0iqnhd64qpdgsb2ihah";
+    sha256 = "06nw1symvif5w0y27p0i7q1aa39jp1s1717c9xdsww62na4n21s8";
   };
 in
 
