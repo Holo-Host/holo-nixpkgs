35531b1f27c59521372ca93b2b742575a1070c0e
Yegor Timoshenko <yegortimoshenko@transumption.com>
2019-11-11 16:06:48 +0000

    holo-router: bump to 7ff48fb73cff1a093898ec783e54d049b3990a9c

--- overlays/holo-nixpkgs/default.nix
+++ overlays/holo-nixpkgs/default.nix
@@ -18,6 +18,13 @@ let
     sha256 = "0jrh5ghisaqdd0vldbywags20m2cxpkbbk5jjjmwaw0gr8nhsafv";
   };
 
+  holo-router = fetchFromGitHub {
+    owner = "Holo-Host";
+    repo = "holo-router";
+    rev = "7ff48fb73cff1a093898ec783e54d049b3990a9c";
+    sha256 = "1mk566ns2nxs20bj6d77mb85l0w8y80prlnxvxijsqpxng65b0b3";
+  };
+
   hpos-state = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "hpos-state";
@@ -60,6 +67,10 @@ in
     cargoToNix;
 
   inherit (callPackage gitignore {}) gitignoreSource;
+
+  inherit (callPackage holo-router {})
+    holo-router-agent
+    holo-router-gateway;
 
   inherit (callPackage hpos-state {})
     hpos-state-derive-keystore
@@ -147,8 +158,6 @@ in
   holoport-nano-dtb = callPackage ./holoport-nano-dtb {
     linux = linux_latest;
   };
-
-  holo-router-gateway = callPackage ./holo-router-gateway {};
 
   holo-auth-client = callPackage ./holo-auth-client {
     stdenv = stdenvNoCC;
