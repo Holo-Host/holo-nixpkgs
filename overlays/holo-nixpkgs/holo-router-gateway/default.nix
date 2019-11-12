6a91c3734fae695b63b5c581998b1a2b6f8fcd64
samrose <samuel.rose@gmail.com>
2019-11-12 16:38:38 -0500

    overlays/holo-nixpkgs/holo-router-gateway: platforms.linux

--- overlays/holo-nixpkgs/holo-router-gateway/default.nix
+++ overlays/holo-nixpkgs/holo-router-gateway/default.nix
@@ -4,8 +4,8 @@
     src = fetchFromGitHub {
       owner = "Holo-Host";
       repo = "holo-router";
-      rev = "061ffd852903c11c27908630f42fb2eba70b0755";
-      sha256 = "00wb10hs8h3k0ivbnk1vixhh9mvhqlmwcxqz7xjswhghh0m8piah";
+      rev = "56cdff53b78423d083ef34e62239b1e8c5d024ce";
+      sha256 = "19x034p81d0fj09vz55hd7sahgr5rcyh2vlyry2yddnk8vq2jwcr";
     };
   in
 
