4d331e46b67aab0907aedc822f6c20db81c70944
PJ <pj@imagine-nyc.com>
2020-02-25 21:13:27 +0100

    holochain-rust: bump to rev 1481cc9

--- overlays/holo-nixpkgs/holochain-rust/default.nix
+++ overlays/holo-nixpkgs/holochain-rust/default.nix
@@ -6,8 +6,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "holochain";
     repo = "holochain-rust";
-    rev = "v0.0.42-alpha5";
-    sha256 = "0x1vnhyq6ksjk2ci7c5kw6n1l8dwfz940sd4vzrcmzl7hhwvpd45";
+    rev = "1481cc930c5c6fdeccb3674efbaed20d135adfeb";
+    sha256 = "01gd19f2xspi07gbnvb3hjlhwrhf6yjhapikrayi4jvp2w603y6g";
   };
 
   cargoSha256 = "1cfk2z4pbk8dli31wpplczd8a1fy8fyhwa5rsl6yz3jzw9d2kdkk";
