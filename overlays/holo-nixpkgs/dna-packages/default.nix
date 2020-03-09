c41a4013f27205e52f98dbadcf2ffd7763a6c5f8
PJ <pj@imagine-nyc.com>
2020-03-09 15:54:05 +0100

    dnaPackages.holofuel: bump to v0.17.0-alpha1

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -4,9 +4,9 @@ with final;
 
 let
   holofuel = fetchurl {
-    url = "https://holo-host.github.io/holofuel/releases/download/v0.16.0-alpha2/holofuel.dna.json";
+    url = "https://holo-host.github.io/holofuel/releases/download/v0.17.0-alpha1/holofuel.dna.json";
     name = "holofuel.dna.json";
-    sha256 = "1pwz9wd0wwg4dj361b3mvmpwp74bgq29z7c5913na8iy87i2mn6s";
+    sha256 = "13zy598afr6npwbfyzxvdgrzwwjaldma6nnk3nzdlxxjdmrqmfyi";
   };
 in
 
