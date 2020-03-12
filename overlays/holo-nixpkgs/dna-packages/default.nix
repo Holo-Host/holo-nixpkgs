2b56843271eb04b71c3d2dd425685c14bf58cc03
zo-el <joelulahanna@gmail.com>
2020-03-12 15:32:39 -0500

    dnaPackages.holofuel: bump dna: Qmf93jJkTrcA7jgvXm4HFXbMLrtdq25fFE18cCeX82nowq

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -6,7 +6,7 @@ let
   holofuel = fetchurl {
     url = "https://github.com/zo-el/temp/releases/download/0.0.2/holofuel.dna.json";
     name = "holofuel.dna.json";
-    sha256 = "1b46adrd7k34zi62as6a6bw0kdqyqww9h19sp57mdkqdwp1p980z";
+    sha256 = "0ha6naadwgxp6a7h8j00671m6ajmj4shr9ccw993c0syr5skqhwb";
   };
 in
 
