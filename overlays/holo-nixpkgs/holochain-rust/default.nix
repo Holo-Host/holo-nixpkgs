6493388d3038c03c8f5807b9ebd0ef87c1135d17
zo-el <joelulahanna@gmail.com>
2020-04-08 16:46:09 -0500

    holochain-rust: bumped to 420fe08c4dfac8f3281e5139e199d7626a142090

--- overlays/holo-nixpkgs/holochain-rust/default.nix
+++ overlays/holo-nixpkgs/holochain-rust/default.nix
@@ -6,8 +6,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "holochain";
     repo = "holochain-rust";
-    rev = "bc43dcf9b4c10b93d4be46e6aaafdfe5e561ae14";
-    sha256 = "123gj6nmzfs3xscma09g95kf5bwz5a4gl61370x71xpwvjbzij5w";
+    rev = "420fe08c4dfac8f3281e5139e199d7626a142090";
+    sha256 = "129qcz1alkn1v1k498qvhvp8g24frh2h3rlj6x7gh9j7d9hk3v8c";
   };
 
   cargoSha256 = "0d7pzs3d42pph1ffm7kyq7qdhzqngvpjr60r82mh37nld3kvrhyz";
