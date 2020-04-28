0b9435dae7e1b5f53070287010de0ca8526bf6ec
zo-el <joelulahanna@gmail.com>
2020-04-28 16:05:51 -0500

    holochain-rust: bumped to dev-test-8

--- overlays/holo-nixpkgs/holochain-rust/default.nix
+++ overlays/holo-nixpkgs/holochain-rust/default.nix
@@ -6,8 +6,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "holochain";
     repo = "holochain-rust";
-    rev = "c7ae7e791c7d23650027bef286e5ff30ec4cea89";
-    sha256 = "0hr70cf5h2xmkzdf204lvzfhfxsvqkslysq8w9pfsim9mkd8x5b4";
+    rev = "3b6486a0bfd5ae03849335c883d3a2887cfe2b3f";
+    sha256 = "11960cfryzi5hahxcric134yniibpynz73x63vnvn2w3ji6n6yls";
   };
 
   cargoSha256 = "032wfgs8w7y3hglf4qs7hggbm139a96a8zchj2lldb80ksikvmpd";
