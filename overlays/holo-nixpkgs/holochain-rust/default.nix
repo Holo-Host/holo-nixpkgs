8e2bc8e8c1d455433a1c4dde6c2fca5790bcd7be
zo-el <joelulahanna@gmail.com>
2020-05-14 18:04:22 -0500

    holochain-rust: bumped to v0.0.48-alpha1

--- overlays/holo-nixpkgs/holochain-rust/default.nix
+++ overlays/holo-nixpkgs/holochain-rust/default.nix
@@ -6,11 +6,11 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "holochain";
     repo = "holochain-rust";
-    rev = "v0.0.46-alpha1";
-    sha256 = "00pw5gn0pwxl67nmj8r0q1s3zbhilis2hvrxvrgachkjmb1a5bj1";
+    rev = "v0.0.48-alpha1";
+    sha256 = "1lzg1s36zycniaarc6j6mkx8bakg5l2n38qxm38wdw0cc67i4xbr";
   };
 
-  cargoSha256 = "0d7pzs3d42pph1ffm7kyq7qdhzqngvpjr60r82mh37nld3kvrhyz";
+  cargoSha256 = "1kj0fyh12lckgb5p09fvh2bh522nsaad6jgm420jx8ihqvx5fn2y";
 
   nativeBuildInputs = [ perl ];
 
