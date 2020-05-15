7bce57e4504582e4d28f3e3981a05db596ef418f
zo-el <joelulahanna@gmail.com>
2020-05-14 23:03:51 -0500

    dnaPackages.holofuel: bumped to v0.20.4-alpha1

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -11,9 +11,9 @@ let
   };
 
   holofuel = fetchurl {
-    url = "https://holo-host.github.io/holofuel/releases/download/v0.20.1/holofuel.dna.json";
+    url = "https://holo-host.github.io/holofuel/releases/download/v0.20.4-alpha1/holofuel.dna.json";
     name = "holofuel.dna.json";
-    sha256 = "159g4d0fhmb4kfi7v4ndizamj6ajfmxxv57ylzkr9q8sdyjb5l8i";
+    sha256 = "16wqjyzdsr7h3knkh612n9m3h9qsdk03h2pl6r32pq788x4acwn2";
   };
 
   holo-hosting-app = fetchFromGitHub {
