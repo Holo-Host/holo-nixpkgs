95dd3630d9b75a254f324e9dec4cf7c966cf9ab3
Alastair Ong <alastair.ong@gmail.com>
2020-05-28 09:04:50 +0100

    Remove happ

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -3,12 +3,6 @@ final: previous:
 with final;
 
 let
-  happ-store = fetchFromGitHub {
-    owner = "holochain";
-    repo = "happ-store";
-    rev = "a7feb1f701753da2b0b3c1de9d6bc1c13896782b";
-    sha256 = "17d94cwk4xgf6i2xx50bxyk1bq68dc1ps4hi9wjy7f1c2qclgfdy";
-  };
 
   holofuel = fetchurl {
     url = "https://holo-host.github.io/holofuel/releases/download/v0.20.1/holofuel.dna.json";
