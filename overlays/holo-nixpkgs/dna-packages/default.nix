52b84799907d4c90f6b5d602b9e4ff8c562ef408
Alastair Ong <alastair.ong@gmail.com>
2020-06-03 17:36:02 +0100

    Just deleting for tests

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -16,27 +16,10 @@ let
     sha256 = "159g4d0fhmb4kfi7v4ndizamj6ajfmxxv57ylzkr9q8sdyjb5l8i";
   };
 
-  holo-hosting-app = fetchFromGitHub {
-    owner = "Holo-Host";
-    repo = "holo-hosting-app";
-    rev = "37013a19c4f2546304e2abea6951e2e06863fcbc";
-    sha256 = "1i43k1wwcpmvx60db2kvbsvfihkfy2nn6iqqwcxz00gqm6pihr2q";
-  };
-
-  servicelogger = fetchFromGitHub {
-    owner = "Holo-Host";
-    repo = "servicelogger";
-    rev = "33fa8304ea00284b73f454d05d61817db53e2869";
-    sha256 = "0b5whd5hgh7zdgf9ppw7z4691ypw5qqim7cv1nx1hqhiyxy8cimh";
-  };
 in
 
 {
   inherit (callPackage happ-store {}) happ-store;
-
-  inherit (callPackage holo-hosting-app {}) holo-hosting-app;
-
-  inherit (callPackage servicelogger {}) servicelogger;
 
   holofuel = wrapDNA holofuel;
 }
