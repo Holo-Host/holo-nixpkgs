261a27456ded7e680cd88f8de85abbf4766d4a84
Alastair Ong <alastair.ong@gmail.com>
2020-06-04 19:11:04 +0100

    test

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -10,19 +10,9 @@ let
     sha256 = "17d94cwk4xgf6i2xx50bxyk1bq68dc1ps4hi9wjy7f1c2qclgfdy";
   };
 
-  holofuel = fetchurl {
-    url = "https://holo-host.github.io/holofuel/releases/download/v0.20.1/holofuel.dna.json";
-    name = "holofuel.dna.json";
-    sha256 = "159g4d0fhmb4kfi7v4ndizamj6ajfmxxv57ylzkr9q8sdyjb5l8i";
-  };
-
-
 in
 
 {
   inherit (callPackage happ-store {}) happ-store;
 
-
-
-  holofuel = wrapDNA holofuel;
 }
