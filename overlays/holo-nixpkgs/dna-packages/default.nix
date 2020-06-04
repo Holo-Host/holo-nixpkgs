a0c14d11ff1dab37a179d3da7bcc3dff2c9c9202
Alastair Ong <alastair.ong@gmail.com>
2020-06-04 10:23:00 +0100

    Remove hApp for test

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -16,13 +16,6 @@ let
     sha256 = "159g4d0fhmb4kfi7v4ndizamj6ajfmxxv57ylzkr9q8sdyjb5l8i";
   };
 
-  holo-hosting-app = fetchFromGitHub {
-    owner = "Holo-Host";
-    repo = "holo-hosting-app";
-    rev = "37013a19c4f2546304e2abea6951e2e06863fcbc";
-    sha256 = "1i43k1wwcpmvx60db2kvbsvfihkfy2nn6iqqwcxz00gqm6pihr2q";
-  };
-
   servicelogger = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "servicelogger";
@@ -33,8 +26,6 @@ in
 
 {
   inherit (callPackage happ-store {}) happ-store;
-
-  inherit (callPackage holo-hosting-app {}) holo-hosting-app;
 
   inherit (callPackage servicelogger {}) servicelogger;
 
