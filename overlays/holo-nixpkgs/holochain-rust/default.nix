8671da22c215c8c2a1c4d7b72773ac80fb819716
zo-el <joelulahanna@gmail.com>
2020-06-06 12:23:14 -0500

    holochain-rust: bumped to 37be0762a62af6d7897877aa5c7dc4afd7648fcc

--- overlays/holo-nixpkgs/holochain-rust/default.nix
+++ overlays/holo-nixpkgs/holochain-rust/default.nix
@@ -6,11 +6,11 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "holochain";
     repo = "holochain-rust";
-    rev = "v0.0.46-alpha1";
-    sha256 = "00pw5gn0pwxl67nmj8r0q1s3zbhilis2hvrxvrgachkjmb1a5bj1";
+    rev = "37be0762a62af6d7897877aa5c7dc4afd7648fcc";
+    sha256 = "0dj42scbai6fq0dhh2gj32426nack40vq25cnfkrnfyfr77jip13";
   };
 
-  cargoSha256 = "0d7pzs3d42pph1ffm7kyq7qdhzqngvpjr60r82mh37nld3kvrhyz";
+  cargoSha256 = "0lxwkj446bhnphspd6dfbanlbhf5rcq8xwavyhnkm0cr14zvba0y";
 
   nativeBuildInputs = [ perl ];
 
