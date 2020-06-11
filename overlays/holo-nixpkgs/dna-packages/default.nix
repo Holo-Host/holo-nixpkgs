f41fd79e7fd8a028d7b2c4cba3536689bc222ba7
Alastair Ong <alastair.ong@gmail.com>
2020-06-11 17:09:00 +0100

    delete service logger

--- overlays/holo-nixpkgs/dna-packages/default.nix
+++ overlays/holo-nixpkgs/dna-packages/default.nix
@@ -22,21 +22,12 @@ let
     rev = "37013a19c4f2546304e2abea6951e2e06863fcbc";
     sha256 = "1i43k1wwcpmvx60db2kvbsvfihkfy2nn6iqqwcxz00gqm6pihr2q";
   };
-
-  servicelogger = fetchFromGitHub {
-    owner = "Holo-Host";
-    repo = "servicelogger";
-    rev = "33fa8304ea00284b73f454d05d61817db53e2869";
-    sha256 = "0b5whd5hgh7zdgf9ppw7z4691ypw5qqim7cv1nx1hqhiyxy8cimh";
-  };
 in
 
 {
   inherit (callPackage happ-store {}) happ-store;
 
   inherit (callPackage holo-hosting-app {}) holo-hosting-app;
-
-  inherit (callPackage servicelogger {}) servicelogger;
 
   holofuel = wrapDNA holofuel;
 }
