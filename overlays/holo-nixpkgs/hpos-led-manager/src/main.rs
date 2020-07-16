f086a27d0d4d4899844b873be2e29caa74b4eff9
Alastair Ong <alastair.ong@gmail.com>
2020-07-16 18:05:12 +0100

    overlays/hpos-led-manager: add check for update status

--- overlays/holo-nixpkgs/hpos-led-manager/src/main.rs
+++ overlays/holo-nixpkgs/hpos-led-manager/src/main.rs
@@ -2,6 +2,7 @@ use aorura::*;
 use docopt::Docopt;
 use failure::*;
 use serde::*;
+use reqwest::Error;
 
 use std::env;
 use std::fs;
@@ -10,6 +11,7 @@ use std::path::{Path, PathBuf};
 use std::thread::sleep;
 use std::time::Duration;
 
+const HYDRA_POLLING_INTERVAL: u64 = 300;
 const POLLING_INTERVAL: u64 = 1;
 
 const USAGE: &'static str = "
@@ -29,6 +31,42 @@ struct Args {
     flag_state: PathBuf,
 }
 
+#[tokio::get_hydra_revision] 
+async fn get_hydra_revision(http_client) { 
+    let channel = fs::read_to_string("/root/.nix-channel") 
+    .expect("Something went wrong reading .nix-channel");
+    let channel_name = channel.split('/').nth(6)                        
+    let eval_url = "https://hydra.holo.host/jobset/holo-nixpkgs/" + channel_name + "/latest-eval"
+
+    http_client
+    .get(eval_url)
+    .header(header::ACCEPT, "application/json")
+    .header(header::CONTENT_TYPE, "application/json")
+    .send()
+    .await?
+    .json()?;
+}
+
+fn request_error_handler(e: reqwest::Error) {
+    if e.is_http() {
+        match e.url() {
+            None => println!("No Url given"),
+            Some(url) => println!("Problem making request to: {}", url),
+        }
+    }
+    // Inspect the internal error and output it
+    if e.is_serialization() {
+       let serde_error = match e.get_ref() {
+            None => return,
+            Some(err) => err,
+        };
+        println!("problem parsing information {}", serde_error);
+    }
+    if e.is_redirect() {
+        println!("server redirecting too many times or making loop");
+    }
+ }
+
 fn main() -> Fallible<()> {
     let args: Args = Docopt::new(USAGE)?
         .argv(env::args())
@@ -41,6 +79,10 @@ fn main() -> Fallible<()> {
     let state_path = args.flag_state;
     let state_temp_path = state_path.with_extension("tmp");
 
+    let client = reqwest::Client::new();
+    let mut counter: u64 = 0;
+    let mut hydra_revision = "none";
+
     loop {
         let router_gateway_addrs = "router-gateway.holo.host:80".to_socket_addrs();
         let online = match router_gateway_addrs {
@@ -52,6 +94,18 @@ fn main() -> Fallible<()> {
         };
 
         let hpos_config_found = Path::new("/run/hpos-init/hpos-config.json").exists();
+
+        if counter % 5000 == 1 {
+            match get_hydra_revision() {
+                Err(e) => request_error_handler(e),
+                Ok(res)  => hydra_revision = res["jobsetevalinputs"]["holo-nixpkgs"]["revision"],
+            };
+            counter += 1;
+        }
+               
+        let local_revision = fs::read_to_string("/root/.nix-revision")
+        .expect("Something went wrong reading nix-revision");
+        let update_required = local_revision == hydra_revision // If this lights up then it's likely the updater isn't working properly
 
         let state = match (online, hpos_config_found) {
             (false, _) => State::Flash(Color::Purple),
