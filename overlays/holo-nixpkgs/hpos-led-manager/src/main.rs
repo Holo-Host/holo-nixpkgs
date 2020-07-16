bc3f6d892a106fea080115b419fbcadb7f050e4d
Alastair Ong <alastair.ong@gmail.com>
2020-07-16 18:08:08 +0100

    overlays/hpos-led-manager: add new LED logic and placeholders for service failure checks

--- overlays/holo-nixpkgs/hpos-led-manager/src/main.rs
+++ overlays/holo-nixpkgs/hpos-led-manager/src/main.rs
@@ -1,8 +1,9 @@
+use anyhow::{Context, Result};
 use aorura::*;
 use docopt::Docopt;
-use failure::*;
+use reqwest::*;
 use serde::*;
-use reqwest::Error;
+use serde_json::Value;
 
 use std::env;
 use std::fs;
@@ -14,7 +15,7 @@ use std::time::Duration;
 const HYDRA_POLLING_INTERVAL: u64 = 300;
 const POLLING_INTERVAL: u64 = 1;
 
-const USAGE: &'static str = "
+const USAGE: &str = "
 Usage: hpos-led-manager --device <path> --state <path>
        hpos-led-manager --help
 
@@ -31,56 +32,43 @@ struct Args {
     flag_state: PathBuf,
 }
 
-#[tokio::get_hydra_revision] 
-async fn get_hydra_revision(http_client) { 
-    let channel = fs::read_to_string("/root/.nix-channel") 
-    .expect("Something went wrong reading .nix-channel");
-    let channel_name = channel.split('/').nth(6)                        
-    let eval_url = "https://hydra.holo.host/jobset/holo-nixpkgs/" + channel_name + "/latest-eval"
+#[tokio::main]
+async fn get_hydra_revision() -> anyhow::Result<serde_json::Value> {
+    let channel_file = fs::read_to_string("/root/.nix-channel")?;
+    let channel_name = channel_file
+        .split('/')
+        .nth(6)
+        .context("invalid hydra url format")?;
+    let eval_url = format!(
+        "https://hydra.holo.host/jobset/holo-nixpkgs/{}/latest-eval",
+        channel_name
+    );
 
-    http_client
-    .get(eval_url)
-    .header(header::ACCEPT, "application/json")
-    .header(header::CONTENT_TYPE, "application/json")
-    .send()
-    .await?
-    .json()?;
+    let res = reqwest::Client::new()
+        .get(&eval_url)
+        .header(header::ACCEPT, "application/json")
+        .header(header::CONTENT_TYPE, "application/json")
+        .send()
+        .await?;
+
+    let json = res.json().await?;
+    Ok(json)
 }
 
-fn request_error_handler(e: reqwest::Error) {
-    if e.is_http() {
-        match e.url() {
-            None => println!("No Url given"),
-            Some(url) => println!("Problem making request to: {}", url),
-        }
-    }
-    // Inspect the internal error and output it
-    if e.is_serialization() {
-       let serde_error = match e.get_ref() {
-            None => return,
-            Some(err) => err,
-        };
-        println!("problem parsing information {}", serde_error);
-    }
-    if e.is_redirect() {
-        println!("server redirecting too many times or making loop");
-    }
- }
-
-fn main() -> Fallible<()> {
+fn main() -> Result<()> {
     let args: Args = Docopt::new(USAGE)?
         .argv(env::args())
         .deserialize()
         .unwrap_or_else(|e| e.exit());
 
-    let mut led = Led::open(&args.flag_device)?;
+    let mut led = Led::open(&args.flag_device).map_err(|e| e.compat())?;
 
     let mut state_prev: State = Default::default();
     let state_path = args.flag_state;
     let state_temp_path = state_path.with_extension("tmp");
 
-    let client = reqwest::Client::new();
     let mut counter: u64 = 0;
+    let mut revision_json;
     let mut hydra_revision = "none";
 
     loop {
@@ -95,29 +83,46 @@ fn main() -> Fallible<()> {
 
         let hpos_config_found = Path::new("/run/hpos-init/hpos-config.json").exists();
 
-        if counter % 5000 == 1 {
-            match get_hydra_revision() {
-                Err(e) => request_error_handler(e),
-                Ok(res)  => hydra_revision = res["jobsetevalinputs"]["holo-nixpkgs"]["revision"],
-            };
-            counter += 1;
+        if counter % HYDRA_POLLING_INTERVAL == 0 {
+            revision_json = get_hydra_revision()?;
+            hydra_revision = revision_json["jobsetevalinputs"]["holo-nixpkgs"]["revision"]
+                .as_str()
+                .context("revision is not a string")?;
+            counter = 0;
         }
-               
+        counter += 1;
+
         let local_revision = fs::read_to_string("/root/.nix-revision")
-        .expect("Something went wrong reading nix-revision");
-        let update_required = local_revision == hydra_revision // If this lights up then it's likely the updater isn't working properly
+            .expect("Something went wrong reading nix-revision");
+        let update_required = local_revision == hydra_revision; // If this lights up then it's likely the updater isn't working properly
 
-        let TLS_certificate: Value = serde_json::from_reader("/var/lib/acme/default/account_reg.json")?; 
-        let TLS_certificate_valid = TLS_certificate["body"]["status"] == "valid" // Note that the TLS_certificate returns a borrow (&value)
+        let tls_certificate_raw = fs::read_to_string("/var/lib/acme/default/account_reg.json")
+            .expect("Something went wrong reading certificate");
+        let tls_certificate: Value = serde_json::from_str(&tls_certificate_raw)?;
+        let tls_certificate_valid = tls_certificate["body"]["status"] == "valid"; // Note that the TLS_certificate returns a borrow (&value)
 
-        let state = match (online, hpos_config_found) {
-            (false, _) => State::Flash(Color::Purple),
-            (true, false) => State::Static(Color::Blue),
+        let system_error = false; // true if any of the following services are in a failed state: holochain-conductor.service, hp-admin-crypto-server.service, hpos-admin.service, nginx.service, zerotierone.service. Important: FAILED only not missing. holochain-conductor might not be on Nano at first.
+        let hosting_error = false; // true if the following services are in a failed state: holo-auth-client.service, hpos-init.service,
+
+        let state = match (
+            system_error,
+            hosting_error,
+            !online,
+            update_required,
+            !hpos_config_found,
+            !tls_certificate_valid,
+        ) {
+            (true, _, _, _, _, _) => State::Flash(Color::Red),
+            (false, true, _, _, _, _) => State::Flash(Color::Yellow),
+            (false, false, true, _, _, _) => State::Flash(Color::Purple),
+            (false, false, false, true, _, _) => State::Static(Color::Blue),
+            (false, false, false, false, true, _) => State::Flash(Color::Green),
+            (false, false, false, false, false, true) => State::Static(Color::Green),
             _ => State::Aurora,
         };
 
         if state != state_prev {
-            led.set(state)?;
+            led.set(state).map_err(|e| e.compat())?;
             state_prev = state;
         }
 
