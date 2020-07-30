728c69abc3e741d4d53b1ee3d2baf6c16fe398cc
Alastair Ong <alastair.ong@gmail.com>
2020-07-30 10:34:03 +0200

    overlays/hpos-admin: add hpos-update to derivation

--- overlays/holo-nixpkgs/hpos-admin/default.nix
+++ overlays/holo-nixpkgs/hpos-admin/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, makeWrapper, python3, hpos-config-is-valid, zerotierone, hpos-reset }:
+{ stdenv, makeWrapper, python3, hpos-config-is-valid, zerotierone, hpos-update-cli, hpos-reset }:
 
 with stdenv.lib;
 
@@ -11,7 +11,7 @@ stdenv.mkDerivation rec {
   buildCommand = ''
     makeWrapper ${python3}/bin/python3 $out/bin/${name} \
       --add-flags ${./hpos-admin.py} \
-      --prefix PATH : ${makeBinPath [ hpos-config-is-valid zerotierone hpos-reset ]}
+      --prefix PATH : ${makeBinPath [ hpos-config-is-valid zerotierone hpos-reset hpos-update-cli ]}
   '';
 
   meta.platforms = platforms.linux;
