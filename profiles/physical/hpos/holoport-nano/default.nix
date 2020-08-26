930e94640b0c70e9a90a08e1be760860c0671a45
Alastair Ong <alastair.ong@gmail.com>
2020-08-26 12:23:47 +0100

    back to S1

--- profiles/physical/hpos/holoport-nano/default.nix
+++ profiles/physical/hpos/holoport-nano/default.nix
@@ -30,7 +30,7 @@
 
   services.automount.enable = true;
 
-  services.hpos-led-manager.devicePath = "/dev/ttyS3";
+  services.hpos-led-manager.devicePath = "/dev/ttyS1";
 
   services.holochain-conductor.enable = lib.mkForce false;
 
