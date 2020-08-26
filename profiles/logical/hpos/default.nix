13127f3181adf927dfd67fbc0555ef930c5f5133
Alastair Ong <alastair.ong@gmail.com>
2020-08-27 00:53:00 +0100

    add aorura-cli for debugging

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -86,7 +86,7 @@ in
 
   environment.noXlibs = true;
 
-  environment.systemPackages = [ hpos-reset bump-dna-cli hpos-admin-client hpos-update-cli git ];
+  environment.systemPackages = [ hpos-reset bump-dna-cli hpos-admin-client hpos-update-cli git aorura-cli ];
 
   networking.firewall.allowedTCPPorts = [ 443 ];
 
