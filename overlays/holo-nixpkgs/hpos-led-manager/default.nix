cd42de0f0e5162aacb804f91433af1d833d08b71
Alastair Ong <alastair.ong@gmail.com>
2020-08-26 13:09:03 +0100

    hpos-led-manager: debug mode

--- overlays/holo-nixpkgs/hpos-led-manager/default.nix
+++ overlays/holo-nixpkgs/hpos-led-manager/default.nix
@@ -6,6 +6,7 @@ rustPlatform.buildRustPackage {
 
   cargoSha256 = "16rz9d7wr9svcnm8jgssdw1kk6vyihw0jl5x8k7nw7jkm64ch123";
   doCheck = false;
+  buildType = "debug";
 
   meta.platforms = lib.platforms.linux;
 }
