b38024bbdf6f75beea57547bf00f914714aa55fe
Joel <joelulahanna@gmail.com>
2020-12-08 13:25:49 -0600

    profiles/logical/hpos: bumped self-hosted-happ to alpha4

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -199,9 +199,9 @@ in
     default-list = [
       {
         app_id = "elemental-chat";
-        version = "alpha3";
-          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha7/elemental-chat.zip ";
-          dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha3/elemental-chat.dna.gz";
+        version = "alpha4";
+          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha8/elemental-chat.zip ";
+          dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha4/elemental-chat.dna.gz";
       }
     ];
   };
