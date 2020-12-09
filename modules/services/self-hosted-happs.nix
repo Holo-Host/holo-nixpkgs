eaa327b61ff18ac1af42834b72ab8e21bb65c8df
peeech <pj@imagine-nyc.com>
2020-12-09 12:05:28 +0100

    services: holochain-configure bindsTo holochain

--- modules/services/self-hosted-happs.nix
+++ modules/services/self-hosted-happs.nix
@@ -29,7 +29,7 @@ in
 
     systemd.services.self-hosted-happs = {
       after = [ "network.target" "holochain.service" ];
-      partOf = [ "holochain.service" ];
+      bindsTo = [ "holochain.service" ];
       wantedBy = [ "multi-user.target" ];
 
       preStart = ''
