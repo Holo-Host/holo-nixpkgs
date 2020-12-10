9dfcdcda37b34ec3b7ee01a70554c57d5c1ab4ea
Joel <joelulahanna@gmail.com>
2020-12-10 09:56:55 -0600

    profiles/logical/hpos: bumped chat to alpha5

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -199,9 +199,9 @@ in
     default-list = [
       {
         app_id = "elemental-chat";
-        version = "alpha4";
-          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha8/elemental-chat.zip ";
-          dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha4/elemental-chat.dna.gz";
+        version = "alpha5";
+          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha9/elemental-chat.zip ";
+          dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha5/elemental-chat.dna.gz";
       }
     ];
   };
