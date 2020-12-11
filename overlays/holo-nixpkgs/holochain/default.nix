440ede1bb0ea2c3a6a556b381257439d75366525
Eric Harris-Braun <eric@harris-braun.com>
2020-12-10 20:32:55 -0500

    update holochain for fix of renewing proxy connection failure

--- overlays/holo-nixpkgs/holochain/default.nix
+++ overlays/holo-nixpkgs/holochain/default.nix
@@ -6,11 +6,11 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "holochain";
     repo = "holochain";
-    rev = "bc1e9b98cc30a0dac8bf8d9a3eee5cc3c5e4e218";
-    sha256 = "0991y090yq2i8q7knzn7ga4iss34qfa2rgd732vhv8pdin33qmv4";
+    rev = "bdd2578a93f81e13d425bd3ed8a6cdc3356f8e1d";
+    sha256 = "1vy1mkbm9hnxxd3j2hpfrjscxwrxj3w38m9cfigpa0vsls2l2x9k";
   };
 
-  cargoSha256 = "01bfmvfxj13vkj1zw79j212ymimk7sh2p6pm059aipzj816dmnrx";
+  cargoSha256 = "0crpxmxp7fq5xq8amls89jjcj0yxn8ijk1bm2v0h97jyb3rq8j6d";
 
   nativeBuildInputs = [ perl pkgconfig ];
 
