3e46f8ad46c0dfc0dcbf88e3b92d5f71bd04dc67
Joel <joelulahanna@gmail.com>
2020-12-15 13:04:42 -0600

    profiles/logical/hpos: happ bumped to alpha9

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -199,9 +199,9 @@ in
     default-list = [
       {
         app_id = "elemental-chat";
-        version = "alpha8";
-          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha12/elemental-chat.zip ";
-          dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha8/elemental-chat.dna.gz";
+        version = "alpha9";
+          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha13/elemental-chat.zip ";
+          dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha9/elemental-chat.dna.gz";
       }
     ];
   };
