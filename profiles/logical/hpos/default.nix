7321647f5b40b03d5fdd3c535c26f6e7e12166a9
Eric Harris-Braun <eric@harris-braun.com>
2020-12-18 15:14:54 -0500

    ec bump for staging--  ui:alpha17 dna:alpha12

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -199,9 +199,9 @@ in
     default-list = [
       {
         app_id = "elemental-chat";
-        version = "alpha11";
-        ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha16/elemental-chat.zip";
-        dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha11/elemental-chat.dna.gz";
+        version = "alpha12";
+        ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha17/elemental-chat.zip";
+        dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha12/elemental-chat.dna.gz";
       }
     ];
   };
