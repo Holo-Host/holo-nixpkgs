d9b0e445342dadc87b202bd4276e0f8238751586
Alastair Ong <alastair.ong@gmail.com>
2021-01-05 13:31:04 +0000

    profiles/match-server: Add Holochain-related services

--- profiles/logical/holo/match-server/default.nix
+++ profiles/logical/holo/match-server/default.nix
@@ -6,6 +6,10 @@ let
   matchServiceApiSocket = "unix:/run/match-service-api.sock";
 
   matchServerCredentialsDir = "/var/lib/match-server-credentials";
+
+  holochainWorkingDir = "/var/lib/holochain-rsm";
+
+  selfHostedHappsWorkingDir = "/var/lib/self-hosted-happs";
 in
 
 {
@@ -13,7 +17,56 @@ in
     ../.
   ];
 
+  environment.systemPackages = [ hc-state git ];
+
   networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.lair-keystore.enable = true;
+
+  services.holochain = {
+    enable = true;
+    working-directory = holochainWorkingDir;
+    config = {
+      environment_path = "${holochainWorkingDir}/databases";
+      keystore_path = "${holochainWorkingDir}/lair-keystore";
+      use_dangerous_test_keystore = false;
+      admin_interfaces = [
+        {
+          driver = {
+            type = "websocket";
+            port = 4444;
+          };
+        }
+      ];
+      network = {
+        bootstrap_service = "https://bootstrap.holo.host";
+        transport_pool = [{
+          type = "proxy";
+          sub_transport = {
+            type = "quic";
+          };
+          proxy_config = {
+            type = "remote_proxy_client";
+            proxy_url = "kitsune-proxy://nFCWLsuRC0X31UMv8cJxioL-lBRFQ74UQAsb8qL4XyM/kitsune-quic/h/proxy.holochain.org/p/5775/--";
+          };
+        }];
+      };
+    };
+  };
+
+  services.self-hosted-happs = {
+    enable = true;
+    working-directory = selfHostedHappsWorkingDir;
+    default-list = [
+      {
+        # Replace this with HHA
+        # app_id = "elemental-chat";
+        # version = "alpha11";
+        # ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha16/elemental-chat.zip";
+        # dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha11/elemental-chat.dna.gz";
+      }
+    ];
+  };
 
   services.kv-uploader = {
     enable = true;
