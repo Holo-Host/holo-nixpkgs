bbac79d4bf6362806f8a84dd603924a6fb374743
Stefan Junker <mail@stefanjunker.de>
2021-01-08 11:58:17 +0100

    configure-holochain: bump 9fb9e4a -> 8ebc86c
    Pulls in the commit that disables holochain's default features.

--- overlays/holo-nixpkgs/configure-holochain/default.nix
+++ overlays/holo-nixpkgs/configure-holochain/default.nix
@@ -7,11 +7,11 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "hpos-configure-holochain";
-    rev = "9fb9e4a20a3c6a274abb4b0093c21b7b00f3955a";
-    sha256 = "0yza86497gqz0932qlh0cngw939aq9w8iza9f26wn1chjigdkjx2";
+    rev = "8ebc86c99ea6ead5d920648c29634bd90a53ec88";
+    sha256 = "0hyg7z8532qjkdh1bk9kxvhs8gs73d4psgqsd9mb59llvl1696z2";
   };
 
-  cargoSha256 = "0vcgbl28c1g78l76rgdnb7akrdb4hcv36niicsr7vx0w7fwr0fj1";
+  cargoSha256 = "0cmi0glnl0awacpr2y7v8i96gjgh000k0lylxccf80vfnhsvi4df";
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ openssl ];
