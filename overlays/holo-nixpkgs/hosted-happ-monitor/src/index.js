dfed1fc82437986b5421bda88145eb50794ef5d1
Alastair Ong <alastair.ong@gmail.com>
2021-01-08 10:17:21 +0000

    overlays/hosted-happs-monitor: add mongodb update functionality

--- overlays/holo-nixpkgs/hosted-happ-monitor/src/index.js
+++ overlays/holo-nixpkgs/hosted-happ-monitor/src/index.js
@@ -1,29 +1,41 @@
-const mongoose = require('mongoose');
-const { AppWebsocket } = require('@holochain/conductor-api')
-const appPort = 42233
-const appId = 'core-hha'
+const fs = require('fs');
+onst MongoClient = require('mongodb').MongoClient;
+const { AppWebsocket } = require('@holochain/conductor-api');
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+const appPort = 42233;
+const appId = 'core-hha';
+
+if (!argv.configPath) {
+  throw new Error('hosted-happ-monitor requires --config-path option.')
+};
+
+const dbName = 'myproject';
+
+// Connection URL
+const url = `mongodb+srv://${username}:${password}@cluster0.hjwna.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const client = new MongoClient(url);
+
+
+
+
+
+
+
 
 const main = async () => {
 
-  mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true,  useUnifiedTopology: true})
-  .then(()=>{
-      console.log("mongodb is connected");
-  })
-  .catch((err)=>{
-      console.log("mongodb not connected");
-      console.log(err);
-  })
-
-  const appWebsocket = await AppWebsocket.connect(`ws://localhost:${appPort}`)
-  const appInfo = appWebsocket.appInfo({ installed_app_id: appId })
+  const appWebsocket = await AppWebsocket.connect(`ws://localhost:${appPort}`);
+  const appInfo = appWebsocket.appInfo({ installed_app_id: appId });
 
   if (!appInfo) {
     throw new Error(`Couldn't find Holo Hosting App with id ${appId}`)
-  }
+  };
 
-  const cellId = appInfo.cell_data[0][0]
+  const cellId = appInfo.cell_data[0][0];
 
-  const agentKey = cellId[1]
+  const agentKey = cellId[1];
 
   const happs = await appWebsocket.callZome({
     cell_id: cellId,
@@ -31,15 +43,28 @@ const main = async () => {
     fn_name: 'get_happs',
     provenance: agentKey,
     payload: {}
-  })
+  });
 
-  const presentedHapps = happs.map(happ => ({
+  const happList = happs.map(happ => ({
     id: happ.happ_id,
     name: happ.happ_bundle.name
-  }))
+  }));
 
+  client.connect(function(err) {
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+    const collection = db.collection('happ_list');
+    
+    await collection.drop()
+    await collection.insertMany(happList, function(err) {
+      if(err) console.log('Error updating happ_list')
+    });
+
+    client.close();
+  });
   
-}
+};
 
 main()
   .then(()=> process.exit())
