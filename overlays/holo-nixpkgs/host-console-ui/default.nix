237965a50aa173cc7a8cfed301e35077fb9fb978
Alastair Ong <alastair.ong@holo.host>
2021-01-30 11:14:25 +0000

    overlays/host-console-ui: bump hash/version for settings

--- overlays/holo-nixpkgs/host-console-ui/default.nix
+++ overlays/holo-nixpkgs/host-console-ui/default.nix
@@ -6,8 +6,8 @@
     src = fetchFromGitHub {
       owner = "holo-host";
       repo = "host-console-ui";
-      rev = "e7fe9587b37dcadb0cd92115bbd267ccd494fe2b";
-      sha256 = "1i5m2qj775ic1p45ysia85kd877c3acgr94cizxpri188hx3k5y3";
+      rev = "eb34155ad23e28bd8e8ca8926c7cfd4050d61e2a";
+      sha256 = "1mllk18qh94rxl2y2fvnzjvpyiv05j8f74sq6idlxli9yjlbzqpk";
     };
 
     packageJSON = "${src}/package.json";
