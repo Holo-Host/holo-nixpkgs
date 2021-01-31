2c8affae74d5bdefa81806c91ccb7d081b8de3c3
Alastair Ong <alastair.ong@holo.host>
2021-01-31 17:09:03 +0000

    profiles/hpos: bump hha version

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -205,8 +205,8 @@ in
         {
           app_id = "core-hha";
           uuid = "0001";
-          version = "0.0.1-alpha5";
-          dna_url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha5/holo-hosting-app.dna.gz";
+          version = "0.0.1-alpha7";
+          dna_url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha7/holo-hosting-app.dna.gz";
         }
       ];
       self_hosted_happs = [
