276fd79b4eba1ce9c313ecfefa264877e8974474
Alastair Ong <alastair.ong@gmail.com>
2021-02-01 19:40:31 +0000

    overlays/host-console-ui: bump ui commit

--- overlays/holo-nixpkgs/host-console-ui/default.nix
+++ overlays/holo-nixpkgs/host-console-ui/default.nix
@@ -6,8 +6,8 @@
     src = fetchFromGitHub {
       owner = "holo-host";
       repo = "host-console-ui";
-      rev = "eb34155ad23e28bd8e8ca8926c7cfd4050d61e2a";
-      sha256 = "1mllk18qh94rxl2y2fvnzjvpyiv05j8f74sq6idlxli9yjlbzqpk";
+      rev = "b4a06ccb73a316fa5738084c809e0d5e8b26d7a0";
+      sha256 = "10i95fkikrs1rdrp5z1bqydx290qlx435l89b8y2ipmnb4ixz46s";
     };
 
     packageJSON = "${src}/package.json";
