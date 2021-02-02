f679b0fd2d4a36244bc6546f0819aea595f85143
Alastair Ong <alastair.ong@gmail.com>
2021-02-02 08:53:04 +0000

    overlays/host-console-ui: another bump :)

--- overlays/holo-nixpkgs/host-console-ui/default.nix
+++ overlays/holo-nixpkgs/host-console-ui/default.nix
@@ -6,8 +6,8 @@
     src = fetchFromGitHub {
       owner = "holo-host";
       repo = "host-console-ui";
-      rev = "13af8511cce1ea61fdddd5bd4f89526737871e2c";
-      sha256 = "1cizp3kdhadyjp5v20jzygryjd2ylfx7x4n0c1vzbv5m6g2cfmr5";
+      rev = "71566d841f5ee676c11577262eba771f2094f2ab";
+      sha256 = "1dq2zf6cs1ssj94nvmpjj6h2m2ay8gr8v8gpp2lpj3dk79m6vb00";
     };
 
     packageJSON = "${src}/package.json";
