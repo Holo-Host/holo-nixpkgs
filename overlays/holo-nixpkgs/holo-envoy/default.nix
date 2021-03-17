6a870a659b6f07d0613ec023bc1ee38a510e4271
timotree3 <timorcb@gmail.com>
2021-03-17 12:57:08 -0400

    overlays/holo-nixpkgs/holo-envoy: Bump envoy

--- overlays/holo-nixpkgs/holo-envoy/default.nix
+++ overlays/holo-nixpkgs/holo-envoy/default.nix
@@ -15,8 +15,8 @@ mkYarnPackage rec {
   src = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "holo-envoy";
-    rev = "7837f452a3edef4221385ad538d61769ddf7643d";
-    sha256 = "0a1465487bkr3dd95mya45hlk6106aakgsxfdzc36ibp53w4rvz9";
+    rev = "c88d52cd8954b78767c4fcbbc98df0645db24292";
+    sha256 = "1z73g3j7zbiwp1y1jnlpg87n0gs20qh582520xbwdm66l24rwpyg";
   };
 
   buildInputs = [ python ];
