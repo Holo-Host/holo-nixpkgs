c0ba6171f7b3e6236c92e8145eeb7de33febf80c
Robbie Carlton <robbie.carlton@gmail.com>
2021-03-31 15:25:58 -0600

    right version of host-console-ui

--- overlays/holo-nixpkgs/host-console-ui/default.nix
+++ overlays/holo-nixpkgs/host-console-ui/default.nix
@@ -6,7 +6,7 @@
     src = fetchFromGitHub {
       owner = "holo-host";
       repo = "host-console-ui";
-      rev = "475f80fe3ef1214a704f5efeba2fc220a9729cc6";
+      rev = "1b18fe215ddcc92e8de47305109a85dd9c7e3981";
       sha256 = "0nfrkq2cj604yb35dy5cb4im4k0sz54m31n45xpkc0x3lrh0yzjg";
     };
 
