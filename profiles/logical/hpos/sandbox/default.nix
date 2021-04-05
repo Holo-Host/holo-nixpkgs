56c2368b3035343526c432266f8433f3025292a5
zo-el <joelulahanna@gmail.com>
2021-04-05 00:56:06 -0500

    profiles/logical/hpos/sandbox: updated dnas

--- profiles/logical/hpos/sandbox/default.nix
+++ profiles/logical/hpos/sandbox/default.nix
@@ -56,35 +56,21 @@ in
     install-list = {
       core_happs = [
         {
-          app_id = "core-happs";
-          uuid = "0001";
-          version = "alpha0";
-          /* dna_url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha6/holo-hosting-app.dna.gz"; */
+          app_id = "core-apps";
           bundle_path = builtins.fetchurl {
-            url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.1.0-alpha1/holo-hosting-app.happ";
-            sha256 = "1mrmvgcx014bg9z4hk4i1ypfq4wwmqh5kgw02pis9yxnlk4jl3sb"; # To get sha run `nix-prefetch-url URL`
+            url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.1.0-alpha3/core-app.0_1_0-alpha3.happ";
+            sha256 = "0i0r1lh83ls2xpik518shfv65fdskqba4gahhd5w1ji38w6m15ld"; # To get sha run `nix-prefetch-url URL`
           };
         }
         {
           app_id = "servicelogger";
-          uuid = "0001";
-          version = "alpha0";
-          /* dna_url = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.0.1-alpha4/servicelogger.dna.gz"; */
           bundle_path = builtins.fetchurl {
-            url = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.1.0-alpha2/servicelogger.happ";
-            sha256 = "0jygfivfbl7v3z85iv5j56s4gd7ww90n98vid1503rxd983ankb2"; # To get sha run `nix-prefetch-url URL`
+            url = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.1.0-alpha4/servicelogger.0_1_0-alpha4.happ";
+            sha256 = "0z128p7pcnay78w4z3cfka55z29win8hdraifz73xnlwq1bm0w3v"; # To get sha run `nix-prefetch-url URL`
           };
         }
       ];
-      self_hosted_happs = [
-        /* {
-          app_id = "elemental-chat";
-          uuid = "0666";
-          version = "alpha14";
-          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha20/elemental-chat.zip";
-          dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha14/elemental-chat.dna.gz";
-          } */
-      ];
+      self_hosted_happs = [];
     };
   };
 
