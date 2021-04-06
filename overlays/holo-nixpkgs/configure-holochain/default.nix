7a77ed154fdb884556e4e9615e8477fbd47fdaa4
zo-el <joelulahanna@gmail.com>
2021-04-06 06:34:08 -0500

    overlays/holo-nixpkgs/configure-holochain: bump to 0b0af

--- overlays/holo-nixpkgs/configure-holochain/default.nix
+++ overlays/holo-nixpkgs/configure-holochain/default.nix
@@ -7,11 +7,11 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "hpos-configure-holochain";
-    rev = "ce60bfa85414a2c0eaa4bd691ae94b56b4895120";
-    sha256 = "061rp2ynxwgraswgrxcyj0iqasvhlyf4nijhy3if557il9j58yzz";
+    rev = "0b0afa8f6832cb56b8e76b0f6fcabe289237c222";
+    sha256 = "061rp2ynxwgraswgrxcyj0iqasvhlyf4nijhy3if557il9j58yza";
   };
 
-  cargoSha256 = "0sa1fzaavrr5x5i6cm2yg4v0anb43rvl8lqqj5rc1ga48pb3b5q3";
+  cargoSha256 = "0sa1fzaavrr5x5i6cm2yg4v0anb43rvl8lqqj5rc1ga48pb3b5qa";
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ openssl ];
