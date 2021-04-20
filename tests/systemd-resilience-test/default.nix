8dd116bd9c980c82d0431ccbb39defda81bc6b45
peeech <pj@imagine-nyc.com>
2021-04-20 11:16:19 +0200

    tests: systemd-resilience-test finished

--- tests/systemd-resilience-test/default.nix
+++ tests/systemd-resilience-test/default.nix
@@ -15,34 +15,37 @@ makeTest {
         "hpos-config-gen-cli --email test\@holo.host --password : --seed-from ${./seed.txt} > /etc/hpos/config.json"
     )
 
-    // wait for configure-holochain
+    # wait for configure-holochain
     machine.wait_for_unit("configure-holochain.service")
     machine.wait_for_open_port("42233")
 
-    // check if happ installed
-    happs = machine.succeed("hc-state -a").strip()
-    print(happs)
+    # check if happ installed
+    happs = machine.succeed("hc-state -a")
+    if "servicelogger" not in happs or "holo-hosting-app" not in happs:
+        raise Exception("Can't find any installed core happs - configure holochain failure?")
 
-    // kill lair & wait for lair and hc
-    lair = get_unit_info("lair-keystore")
+    # kill lair & wait for lair and hc
+    lair = machine.get_unit_info("lair-keystore")
     machine.succeed(make_command(["kill", "-9", lair["MainPID"]]))
     machine.wait_for_unit("lair-keystore.service")
     machine.wait_for_unit("holochain.service")
 
-    // kill holo-envoy & wait for envoy and holochain
-    envoy = get_unit_info("holo-envoy")
+    # kill holo-envoy & wait for envoy and holochain
+    envoy = machine.get_unit_info("holo-envoy")
     machine.succeed(make_command(["kill", "-9", envoy["MainPID"]]))
     machine.wait_for_unit("holo-envoy.service")
     machine.wait_for_unit("holochain.service")
 
-    // kill holochain & wait for holochain
-    holochain = get_unit_info("holochain")
+    # kill holochain & wait for holochain
+    holochain = machine.get_unit_info("holochain")
     machine.succeed(make_command(["kill", "-9", holochain["MainPID"]]))
     machine.wait_for_unit("holochain.service")
 
-    // Check if happs are the same
-    happs2 = machine.succeed("hc-state -a").strip()
-    print(happs2)
+    # Final checks
+    machine.wait_for_open_port("42233")
+    happs2 = machine.succeed("hc-state -a")
+    if "servicelogger" not in happs or "holo-hosting-app" not in happs2:
+        raise Exception("Can't find any installed core happs - configure holochain failure?")
 
     machine.shutdown()
   '';
