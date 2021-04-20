2e9817910f487431b0e90b0350a09766c8103273
Rob <sir.rob.lyon@gmail.com>
2021-04-21 00:33:59 +0100

    change uid from 0003 to 0002

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -223,7 +223,7 @@ in
     };
   };
 
-  systemd.globalEnvironment.DEV_UID_OVERRIDE = "0003";
+  systemd.globalEnvironment.DEV_UID_OVERRIDE = "0002";
 
   services.configure-holochain = lib.mkDefault {
     enable = true;
