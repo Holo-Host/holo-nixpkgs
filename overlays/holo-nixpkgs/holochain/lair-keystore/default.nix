4118d1d538a32449391f57da325cef8f7346a71f
Rob <sir.rob.lyon@gmail.com>
2021-04-21 00:07:52 +0100

    fix copy paste error in cargosha update

--- overlays/holo-nixpkgs/holochain/lair-keystore/default.nix
+++ overlays/holo-nixpkgs/holochain/lair-keystore/default.nix
@@ -15,7 +15,7 @@ rustPlatform.buildRustPackage {
     sha256 = "05p8j1yfvwqg2amnbqaphc6cd92k65dq10v3afdj0k0kj42gd6ic";
   };
 
-  cargoSha256 = "1x7gzndv8qax3wwv7imki9rrzm0l22qhf49bdkjjn6nb430fmln";
+  cargoSha256 = "1x7gzndv8qax3wwv7imki9rrzm0l22qhf49bdkjjn6nb430fmlnk";
 
   buildInputs = lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
     AppKit
