0531613c48702b91acabb2fcdfd08fff4c69d88d
peeech <pj@imagine-nyc.com>
2021-04-23 12:00:37 +0200

    holochain-proxy: create service and profile

---
+++ profiles/logical/holo/holochain-proxy/default.nix
@@ -0,0 +1,12 @@
+{
+  imports = [ ../. ];
+
+  services.holochain-proxy.enable = {
+    enable = true;
+    cert-file = "proxy.cert";
+    working-directory = "/var/lib/holochain-proxy";
+    port = 5779;
+  };
+
+  system.defaultChannel = lib.mkDefault "https://hydra.holo.host/channel/custom/holo-nixpkgs/develop/holo-nixpkgs";
+}
