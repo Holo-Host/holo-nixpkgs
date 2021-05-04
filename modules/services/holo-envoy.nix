befb438d176484bda653d7d764c963887436d576
Alastair Ong <alastair.ong@holo.host>
2021-05-04 09:50:05 +0100

    modules/holo-envoy: change log level to silly

--- modules/services/holo-envoy.nix
+++ modules/services/holo-envoy.nix
@@ -29,7 +29,7 @@ in
       '';
 
       serviceConfig = {
-        Environment = "LOG_LEVEL=debug";
+        Environment = "LOG_LEVEL=silly";
         ExecStart = "${cfg.package}/bin/holo-envoy";
         TimeoutStartSec = 300;
         Restart = "always";
