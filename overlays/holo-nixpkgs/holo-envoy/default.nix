a8d89ac077659b7622c2d3be26f62b788d33a4cb
jetttech <ldjetton21@gmail.com>
2021-05-25 10:52:31 -0500

    bump holo-envoy

--- overlays/holo-nixpkgs/holo-envoy/default.nix
+++ overlays/holo-nixpkgs/holo-envoy/default.nix
@@ -15,8 +15,8 @@ mkYarnPackage rec {
   src = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "holo-envoy";
-    rev = "7d876e4ed1a68ae2de364debc4ec8d6dc191fb3d";
-    sha256 = "12jca3b6gm804wragqb508l40j7c3shlbi4kcicpssbxzlqsjlc1";
+    rev = "5c877b0d85cfbeb981a244142b3de99ec12cc651";
+    sha256 = "108j7wri0cxgg9mj24mblakhl464y7ab9z023hf25j4qdr3qlbq3";
   };
 
   buildInputs = [ python ];
