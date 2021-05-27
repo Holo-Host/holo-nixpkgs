9f6103be4cf5fe849f13b4ed294a068b84e80a04
Alastair Ong <alastair.ong@holo.host>
2021-05-27 17:01:52 +0100

    overlays/hosted-happ-monitor: update for happ bundle

--- overlays/holo-nixpkgs/hosted-happ-monitor/src/const.js
+++ overlays/holo-nixpkgs/hosted-happ-monitor/src/const.js
@@ -7,20 +7,19 @@ const CONFIGURE_HC = process.env.NODE_ENV === 'test' ? './tests/config.yml' : '/
 
 const getAppIds = async () => {
   try {
-    let config = await yaml.load(fs.readFileSync(CONFIGURE_HC, 'utf8'))
-    const getId = (id) => {
-      let app = config.core_happs.find(h => h.app_id == id)
-      if (app.uuid === undefined) {
-        return `${id}:${app.version}`
-      } else {
-        return `${id}:${app.version}:${app.uuid}`
+    const config = yaml.load(fs.readFileSync(CONFIGURE_HC, 'utf8'))
+    const getId = (name) => {
+      const { bundle_url } = config.core_happs.find(h => h.bundle_url.includes(name))
+      const bundleUrlPath = new URL(bundle_url).pathname
+      const id =  bundleUrlPath.slice(bundleUrlPath.lastIndexOf('/') + 1)
+        .replace('.happ', '')
+        .replace('.', ':')
+      if (DEV_UID_OVERRIDE) {
+        return `${id}::${DEV_UID_OVERRIDE}`
       }
+      return id
     }
-    if (process.env.NODE_ENV === 'test') return {
-      HHA: config[0].app_name,
-      SL: config[1].app_name
-    }
-    else return {
+    return {
       HHA: getId('core-app'),
       SL: getId('servicelogger')
     }
@@ -28,7 +27,6 @@ const getAppIds = async () => {
     throw new Error(e)
   }
 }
-
 module.exports = {
   HAPP_PORT,
   getAppIds
