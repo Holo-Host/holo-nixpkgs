2d14b21731201b2e632c9e344218358200dd6de3
Alastair Ong <alastair.ong@holo.host>
2021-05-27 14:22:09 +0100

    overlays/hosted-happ-monitor: update core-app name

--- overlays/holo-nixpkgs/hosted-happ-monitor/src/const.js
+++ overlays/holo-nixpkgs/hosted-happ-monitor/src/const.js
@@ -21,7 +21,7 @@ const getAppIds = async () => {
       SL: config[1].app_name
     }
     else return {
-      HHA: getId('core-happs'),
+      HHA: getId('core-apps'),
       SL: getId('servicelogger')
     }
   } catch (e) {
