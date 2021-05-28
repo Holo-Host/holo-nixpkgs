79bc4beaf00193a32c6da44023fe0d59d77a378c
Alastair Ong <alastair.ong@holo.host>
2021-05-28 09:00:02 +0100

    overlays/hosted-happ-monitor: use dev override

--- overlays/holo-nixpkgs/hosted-happ-monitor/src/const.js
+++ overlays/holo-nixpkgs/hosted-happ-monitor/src/const.js
@@ -5,6 +5,8 @@ const HAPP_PORT = 42233;
 
 const CONFIGURE_HC = process.env.NODE_ENV === 'test' ? './tests/config.yml' : '/var/lib/configure-holochain/config.yaml';
 
+const DEV_UID_OVERRIDE = process.env.DEV_UID_OVERRIDE
+
 const getAppIds = async () => {
   try {
     const config = yaml.load(fs.readFileSync(CONFIGURE_HC, 'utf8'))
