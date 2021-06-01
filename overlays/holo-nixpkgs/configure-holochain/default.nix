be3d7baecfb083d4eb5da26d10926237b424b5e0
Alastair Ong <alastair.ong@holo.host>
2021-06-01 13:35:15 +0100

    overlays/configure-holochain: bump to version that works with new wasmer

--- overlays/holo-nixpkgs/configure-holochain/default.nix
+++ overlays/holo-nixpkgs/configure-holochain/default.nix
@@ -7,11 +7,11 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "hpos-configure-holochain";
-    rev = "6c6ae85d461ed500020173b83802788e0f96c3ae";
-    sha256 = "1kddb42cxdiwrhzdws94hnylb8k21bsl58w6j96z9ahkqd9dscc6";
+    rev = "31a339e961bbe7b9177f3a721b5d29801c0d274b";
+    sha256 = "1m68nqww9gz2hki4n3xv86c6wr1kgf7snbbf7nnsbb26dhb5qlhm";
   };
 
-  cargoSha256 = "1v41g8sv7ig7nxn1jvpvigy75frwaff2wysr4pf4jiqvrwn68dmp";
+  cargoSha256 = "1n9pywziayyhvr2sjp595psh5niahvdl2d1lmblklkvarbp58c1d";
 
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ openssl ];
