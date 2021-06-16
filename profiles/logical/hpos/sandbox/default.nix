f01813476042593e0e8e5aeae8818031d31bfbc9
Alastair Ong <alastair.ong@holo.host>
2021-06-16 10:46:03 +0100

    profiles/hpos: bump to HHA alpha19

--- profiles/logical/hpos/sandbox/default.nix
+++ profiles/logical/hpos/sandbox/default.nix
@@ -59,8 +59,8 @@ in
         {
           app_id = "core-app"; # not used, just for clarity here
           bundle_path = builtins.fetchurl {
-            url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/0_1_0_alpha17/core-app.0_1_0_alpha17.happ";
-            sha256 = "0fxijd1nxyqp1ca933n04sv8xjl3hdhz86xgbz0mkr28lfmrb12x"; # To get sha run `nix-prefetch-url URL`
+            url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/0_1_0_alpha19/core-app.0_1_0_alpha19.happ";
+            sha256 = "1a3sh9w36npqsag28qmzhawd3ik2zqm21b65ifcvpww4r9ilrdql"; # To get sha run `nix-prefetch-url URL`
           };
         }
         {
