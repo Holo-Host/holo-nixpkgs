a6440c7cc84826e5e0b133a6b9b123e9021f2bab
Alastair Ong <alastair.ong@holo.host>
2021-06-22 16:38:38 +0100

    overlays/joining-code-factory: fix derivation

--- overlays/holo-nixpkgs/joining-code-factory/default.nix
+++ overlays/holo-nixpkgs/joining-code-factory/default.nix
@@ -1,16 +1,64 @@
-{ stdenv, fetchFromGitHub, mkYarnPackage }:
+# { stdenv, fetchFromGitHub, mkYarnPackage }:
 
+# {
+#   joining-code-factory = mkYarnPackage rec {
+#     name = "joining-code-happ";
+#     src = fetchFromGitHub {
+#       owner = "holo-host";
+#       repo = "joining-code-happ";
+#       rev = "a96bce045d53637b3893fc1e486ac5607fa84577";
+#       sha256 = "0wx9s98h5gjb3ybc300113zvmzz0y03f5sb7vpp6pqbwy5hk57ac";
+#       private = true;
+#     };
+
+#     packageJSON = "${src}/service/package.json";
+#     # yarnLock = "${src}/service/yarn.lock";
+#   };
+
+#   buildPhase = ''
+#     echo "BLAH!!!!
+#   '';
+
+#   installPhase = ''
+#     mkdir $out
+#     mv build/bundle.js $out
+#   '';
+
+# }
+
+{ stdenv, nodejs, npmToNix, fetchFromGitHub }:
+
+with stdenv.lib;
 {
-  joining-code-factory = mkYarnPackage rec {
-    name = "joining-code-factory";
+  joining-code-factory = stdenv.mkDerivation rec {
+    name = "joining-code-happ";
     src = fetchFromGitHub {
-      owner = "holo-host";
-      repo = "joining-code-happ";
-      rev = "a96bce045d53637b3893fc1e486ac5607fa84577";
-      sha256 = "12fvyqmpqi4xmq2x5ydb92bdnilwnvb5d5iykzzl0g8ak2w2arjp";
-    };
+        owner = "holo-host";
+        repo = "joining-code-happ";
+        rev = "a96bce045d53637b3893fc1e486ac5607fa84577";
+        sha256 = "0wx9s98h5gjb3ybc300113zvmzz0y03f5sb7vpp6pqbwy5hk57ac";
+        private = true;
+      };
 
-    packageJSON = "${src}/package.json";
-    yarnLock = "${src}/yarn.lock";
+    buildInputs = [ nodejs ];
+
+    preConfigure = ''
+      cp -r ${npmToNix { src = "${src}/service"; }} ./service/node_modules
+      chmod -R +w ./service/node_modules
+      patchShebangs ./service/node_modules
+    '';
+
+    buildPhase = ''
+      cd ./service
+      npm run build
+    '';
+
+    installPhase = ''
+      ls -l
+      mkdir $out
+      mv build/bundle.js $out
+    '';
+
+    meta.platforms = platforms.linux;
   };
 }
