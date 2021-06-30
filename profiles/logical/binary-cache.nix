5006ed8b79ca24551e3c19d41e78b99ebe944525
peeech <pj@imagine-nyc.com>
2021-06-30 14:22:35 +0200

    caches: cache.holo.host->cache-s3.holo.host

--- profiles/logical/binary-cache.nix
+++ profiles/logical/binary-cache.nix
@@ -1,11 +1,9 @@
 {
   nix.binaryCaches = [
-    "https://cache.holo.host/"
+    "https://cache-s3.holo.host/"
   ];
 
   nix.binaryCachePublicKeys = [
-    # deprecated
-    "cache.holo.host-1:lNXIXtJgS9Iuw4Cu6X0HINLu9sTfcjEntnrgwMQIMcE="
     "cache.holo.host-2:ZJCkX3AUYZ8soxTLfTb60g+F3MkWD7hkH9y8CgqwhDQ="
   ];
 }
