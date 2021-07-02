3a4d477aca6a3ef2f74e96b78860ca1d1d305ee1
peeech <pj@imagine-nyc.com>
2021-07-02 12:27:20 +0200

    profiles/holo-networks: create testnet holo network

--- profiles/logical/holo-networks.nix
+++ profiles/logical/holo-networks.nix
@@ -8,9 +8,9 @@
       stamp = "sdns://AgcAAAAAAAAADTEwNC4xNy4yNDEuNDUAGXJvdXRlci1yZWdpc3RyeS5ob2xvLmhvc3QNL3YxL2Rucy1xdWVyeQ";
       url = "https://router-registry.holo.host";
     };
-    bootstrapUrl = "https://devnet-bootstrap.holo.host";
+    bootstrapUrl = "https://bootstrap.holo.host";
     proxy = rec {
-      ipAddress = "167.172.0.245";
+      ipAddress = "45.55.107.33";
       port = "5788";
       pubKey = "f3gH2VMkJ4qvZJOXx0ccL_Zo5n-s_CnBjSzAsEHHDCA";
       certFile = "proxy.cert";
@@ -25,6 +25,24 @@
     routerRegistry = {
       stamp = "sdns://AgcAAAAAAAAADTEwNC4xNy4yNDEuNDUAIGRldm5ldC1yb3V0ZXItcmVnaXN0cnkuaG9sby5ob3N0DS92MS9kbnMtcXVlcnk";
       url = "https://devnet-router-registry.holo.host";
+    };
+    bootstrapUrl = "https://devnet-bootstrap.holo.host";
+    proxy = rec {
+      ipAddress = "167.172.0.245";
+      port = "5788";
+      pubKey = "f3gH2VMkJ4qvZJOXx0ccL_Zo5n-s_CnBjSzAsEHHDCA";
+      certFile = "proxy.cert";
+      kitsuneAddress = "kitsune-proxy://${pubKey}/kitsune-quic/h/${ipAddress}/p/${port}/--";
+    };
+  };
+
+  testnet = {
+    networkName = "testNet";
+    zerotierNetworkID = "93afae5963c547f1";
+    hposDomain = "holohost.net";
+    routerRegistry = {
+      stamp = "sdns://AgcAAAAAAAAADTEwNC4xNy4yNDEuNDUAGXJvdXRlci1yZWdpc3RyeS5ob2xvLmhvc3QNL3YxL2Rucy1xdWVyeQ";
+      url = "https://router-registry.holo.host";
     };
     bootstrapUrl = "https://devnet-bootstrap.holo.host";
     proxy = rec {
