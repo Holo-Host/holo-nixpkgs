42257ffb4f05cf2f494837ecde0abbdedb43b0b9
Alastair Ong <alastair.ong@holo.host>
2021-07-06 17:30:54 +0100

    modules: add ssh-pinger

---
+++ modules/services/ssh-pinger.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.services.ssh-pinger;
+in
+
+{
+  options.services.ssh-pinger = {
+    enable = mkEnableOption "test-hp-manager";
+
+    package = mkOption {
+      default = pkgs.test-hp-manager;
+      type = types.package;
+    };
+
+    credentialsDir = mkOption {
+      type = types.path;
+    };
+  };
+
+  config = mkIf (cfg.enable) {
+    environment.systemPackages = [ cfg.package pkgs.nodejs ];
+
+    systemd.services.ssh-pinger = {
+      after = [ "network.target" "holochain.service" "configure-holochain.service"];
+      requisite = [ "holochain.service" ]; 
+      
+      serviceConfig = {
+        ExecStart = "${pkgs.nodejs}/bin/node --no-warnings ${cfg.package}/bin/test-holoports-monitor --ssh-key-path=${cfg.credentialsDir}/id_rsa --config-path=${cfg.credentialsDir}/config.json";
+        Type = "oneshot";
+        User = "root";
+        unitConfig.X-StopOnRemoval = false;
+        restartIfChanged = false;
+      };
+    };
+
+    systemd.timers.ssh-pinger = {
+      enable = true;
+      wantedBy = [ "multi-user.target" ];
+
+      timerConfig = {
+        OnUnitActiveSec = "1 h";
+        OnBootSec = "10sec";
+        Unit = "ssh-pinger.service";
+      };
+    };
+  };
+}
